f35a5e315ce05a031436370befb3fc1799c7873e
Martijn339074 <339074@student.mboutrecht.nl>
2024-11-25 14:57:52 +0100

    added form

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tandartspraktijk</title>
+    <title>SmilePro</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.2/dist/tailwind.min.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
@@ -11,7 +11,7 @@
     <!-- Header -->
     <header class="bg-blue-800 text-white shadow-md">
         <div class="container mx-auto flex justify-between items-center py-4 px-6">
-            <h1 class="text-2xl font-bold">Tandartspraktijk</h1>
+            <h1 class="text-2xl font-bold">SmilePro</h1>
             <p class="text-sm italic">Uw glimlach, onze zorg</p>
         </div>
     </header>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/Appointment', function () {
+    return view('Appointment');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
