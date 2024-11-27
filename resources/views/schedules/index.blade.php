e769cbbcb61514e6132955825912f5d1a7d80f54
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-27 13:55:23 +0100

    views Schedules CRUD pages

--- resources/views/schedules/index.blade.php
+++ resources/views/schedules/index.blade.php
@@ -1,17 +1,20 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <h1>Schedules</h1>
-    <a href="{{ route('schedules.create') }}" class="btn btn-primary">Create New Schedule</a>
-    <ul>
-        @foreach($schedules as $schedule)
-            <li>
-                <a href="{{ route('schedules.show', $schedule->id) }}">{{ $schedule->title }}</a>
-                <p><strong>Start Time:</strong> {{ $schedule->start_time }}</p>
-                <p><strong>End Time:</strong> {{ $schedule->end_time }}</p>
-            </li>
-        @endforeach
-    </ul>
-</div>
-@endsection
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-200 leading-tight">
+            {{ __('Schedules') }}
+        </h2>
+    </x-slot>
+    <div class="container">
+        <h1>Schedules</h1>
+        <a href="{{ route('schedules.create') }}" class="btn btn-primary">Create New Schedule</a>
+        <ul>
+            @foreach ($schedules as $schedule)
+                <li>
+                    <a href="{{ route('schedules.show', $schedule->id) }}">{{ $schedule->title }}</a>
+                    <p><strong>Start Time:</strong> {{ $schedule->start_time }}</p>
+                    <p><strong>End Time:</strong> {{ $schedule->end_time }}</p>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</x-app-layout>
