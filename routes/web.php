d706e488b9fecf2fea9e7f263c74f01c9b7356f7
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-28 12:58:41 +0100

    create form

--- routes/web.php
+++ routes/web.php
@@ -66,7 +66,7 @@ Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit')
 Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-// Route::resource('schedules', ScheduleController::class)->middleware('auth');
+Route::resource('schedules', ScheduleController::class)->middleware('auth');
 
 Route::middleware(['auth', AdminMiddleware::class])->group(function () {
     Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
