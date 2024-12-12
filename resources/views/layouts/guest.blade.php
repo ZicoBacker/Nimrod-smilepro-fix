bac4c4c5d3fe5aafc53a22c01cb98851d323f6ec
ThomasTadesse <328704@student.mboutrecht.nl>
2024-12-12 12:47:18 +0100

    color login & reg

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,8 +17,8 @@
     @vite(['resources/js/app.js'])
 </head>
 
-<body class="font-sans text-gray-900 antialiased">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+<body style="background-color: #54b9bd; color: white;" class="shadow-md">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="background-color: #54b9bd;">
         <div>
             <a href="/">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
